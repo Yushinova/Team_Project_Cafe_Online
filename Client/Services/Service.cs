d866dc39e4f6a8eebce28d1b6fe1121e8165af72
Yushi_np28@top-academy.ru <Yushi_np28@top-academy.ru>
2024-08-18 16:28:03 +0300

    add service

---
+++ Client/Services/Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Services
+{
+    public class Service
+    {
+        public string IP = "127.0.0.1";
+        public int port = 8888;
+
+    }
+}
